eb169476ab3f20c315bc011e72ddcf4877ee6fd4
hyungwoo1413 <glhwguddn@gmail.com>
2025-03-11 15:13:29 +0900

    2025-03-11

--- Day1/HelloWorld.c
+++ Day1/HelloWorld.c
@@ -2,7 +2,7 @@
 
 int HelloWorld()			// 함수: 이름 뒤에 괄호가 있으면 무조건 함수다.
 {
-	printf("Hello World!!");	// 함수호출
+	printf("Hello World!!!");	// 함수호출
 
 	return 0; // 주석
 }
--- Day1/value4.c
+++ Day1/value4.c
@@ -9,6 +9,11 @@ int value4()
 	printf("str[1]: %c\n", str[1]);
 	printf("str[5]: %c\n", str[5]);
 	// printf("str[6]: %c\n", str[6]);  // 문자열 끝에는 null문자가 온다
+	
+	char str2[6] = "apple";
+	printf("str2: %s\n", str2);
+	str2[0] = 'A';
+	printf("str2: %s", str2);
 
 	return 0;
 }
