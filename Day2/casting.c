541fac5dfc3108057f34e751d1fa35a3a0b5a9b7
hyungwoo1413 <glhwguddn@gmail.com>
2025-03-12 13:40:36 +0900

    2025-03-12

---
+++ Day2/casting.c
@@ -0,0 +1,13 @@
+﻿//#include <stdio.h>
+//
+//int main() {
+//	int n = 10;
+//	double res;
+//
+//	// res = (double)n / 3;
+//	// res = n / 3.0;  // 둘 중 하나만 실수형이면 결과는 실수로
+//	res = (double)n / 3.0; // 둘 다 실수형일 필요는 없다
+//	printf("res: %.3lf\n", res);
+//
+//	return 0;
+//}
---
+++ Day2/if.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int num;
+	int r = 47;
+
+	while (1) {
+		printf("정수를 입력하세요(1~99): ");
+		scanf_s("%d", &num);
+
+		if (r < num) {
+			printf("down\n");
+		}
+		else if(r > num) {
+			printf("up\n");
+		}
+		else {
+			printf("정답입니다.\n");
+			break;
+		}
+	}
+	
+	return 0;
+}
