93348faffcd0095a55d9f3f0cf49dc371a6e3cd8
hyungwoo1413 <glhwguddn@gmail.com>
2025-03-18 17:30:54 +0900

    2025-03-18

---
+++ Day5/doodle.c
@@ -0,0 +1,24 @@
+﻿//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main() {
+//    int a[] = { 1,2,3,4 };
+//    char c[] = {"ABC", "abc", "hello"};
+//    
+//    void* ptr[];
+//
+//    ptr = malloc(sizeof(int));
+//
+//    if (ptr == NULL) {
+//        printf("메모리 할당 실패!\n");
+//        return 1;
+//    }
+//    *(int*)ptr = 100;
+//    printf("값: %d\n", *(int*)ptr);
+//
+//    ptr = malloc(sizeof(char));
+//
+//
+//    free(ptr);
+//    return 0;
+//}
